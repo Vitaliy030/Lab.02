251ac097c8c313df2b451320eb9466743064037b
Vitaliy030 <60977465+Vitaliy030@users.noreply.github.com>
2020-04-20 23:23:31 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] a = { 5, 4, -9, 5, 4, 3, -5, 2, -1 };
+            int n = 9;
+            int min = -1;
+            double result = ConsoleApp3._1.Program.min_element(a, n);
+            Assert.AreEqual(min, result);
+        }
+    }
+}
